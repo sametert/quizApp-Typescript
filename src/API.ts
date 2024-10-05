19968da70709b199df239dbc472c17a02232a051
Samet <77546980+sametert@users.noreply.github.com>
2024-10-05 12:11:31 +0300

    update api

--- src/API.ts
+++ src/API.ts
@@ -22,7 +22,7 @@ export enum Difficulty {
 export const fetchQuizQuestion = async (amount: number, difficulty: Difficulty) => {
     const endpoint = `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple`;
     const data = await (await fetch(endpoint)).json();
-    // console.log(data);
+    console.log(data);
     return data.results.map((question : Question) => (
         {
             ...question,
